7d12d972b76bf7e67fe5b446b5b287005f9ce704
Isabella Bosia <izaberina@gmail.com>
2026-05-04 19:38:26 +0100

    more benchmarks

--- mca.awk
+++ mca.awk
@@ -7,8 +7,24 @@ BEGIN {
 
 /Code Region/       { region      = $NF }
 /IPC/               { ipc         = $NF }
-/Block RThroughput/ { rthroughput = $NF
-                      printf "%-15s | %15s | %15s\n",
-                             region, rthroughput, ipc }
+/Block RThroughput/ { rthroughput = $NF }
 
+/Block RThroughput/ && region ~ FILTER {
+    printf "%-15s | %15s | %15s\n", region, rthroughput, ipc
+    result[0][region] = rthroughput
+    result[1][region] = ipc
+}
+
+END {
+    print "----------------+-----------------+----------------"
+
+    PROCINFO["sorted_in"] = "@val_type_asc"
+    best = 10000000000
+    for (region in result[0]) {
+        if (result[0][region] <= best) {
+            best = result[0][region]
+            printf "%-15s | %15s | %15s\n", region, result[0][region], result[1][region]
+        }
+    }
+}
 
--- vec.cpp
+++ vec.cpp
@@ -1,4 +1,5 @@
 #include <utility>
+#include <type_traits>
 #include "types.hpp"
 
 #define MCA(name, ...) \
@@ -12,49 +13,99 @@ auto unroller = []<int Unroll>(auto lambda) {
     }(lambda, std::make_index_sequence<Unroll>{});
 };
 
-#define f(t,lanes,unroll)                                        \
-void f_##t##x##lanes##_##unroll(                                 \
-        t##x##lanes *__restrict x, t##x##lanes *__restrict y) {  \
-    MCA(f_##t##x##lanes##_##unroll,                              \
-        for (auto i = 0; i < 256 / lanes; i += unroll) {         \
-            unroller.operator()<unroll>([&](auto j) {            \
-                if constexpr (std::is_same_v<t,i32>)             \
-                    x[i+j] += (y[i+j] - x[i+j]) >> 16;           \
-                if constexpr (std::is_same_v<t,f32>)             \
-                    x[i+j] += (y[i+j] - x[i+j]) / (1<<16);       \
-            });                                                  \
-        }                                                        \
-    )                                                            \
+template <typename T> constexpr int shift_for = 4 * sizeof(T);
+
+#define f(t,lanes,unroll)                                                  \
+void f_##t##x##lanes##_##unroll(                                           \
+        t##x##lanes *__restrict x, t##x##lanes *__restrict y) {            \
+    constexpr int sh = shift_for<t>;                                       \
+    MCA(f_##t##x##lanes##_##unroll,                                        \
+        for (auto i = 0; i < 256 / lanes; i += unroll) {                   \
+            unroller.operator()<unroll>([&](auto j) {                      \
+                if constexpr (std::is_integral_v<t>)                       \
+                    x[i+j] += (y[i+j] - x[i+j]) >> sh;                     \
+                else                                                       \
+                    x[i+j] += (y[i+j] - x[i+j]) / (1 << sh);               \
+            });                                                            \
+        }                                                                  \
+    )                                                                      \
 }
 
-f(i32,8, 1);
-f(i32,8, 2);
-f(i32,8, 4);
-f(i32,8, 8);
-f(i32,8,16);
+// f(i32,8, 1);
+// f(i32,8, 2);
+// f(i32,8, 4);
+// f(i32,8, 8);
+// f(i32,8,16);
 f(i32,8,32);
 
-f(f32,8, 1);
-f(f32,8, 2);
-f(f32,8, 4);
-f(f32,8, 8);
-f(f32,8,16);
+// f(f32,8, 1);
+// f(f32,8, 2);
+// f(f32,8, 4);
+// f(f32,8, 8);
+// f(f32,8,16);
 f(f32,8,32);
 
-#ifdef __AVX512F__
-f(i32,16, 1);
-f(i32,16, 2);
-f(i32,16, 4);
-f(i32,16, 8);
+// #ifdef __AVX2__
+// f(i16,16, 1);
+// f(i16,16, 2);
+// f(i16,16, 4);
+// f(i16,16, 8);
+f(i16,16,16);
+// #endif
+
+// #ifdef __AVX512F__
+// f(i32,16, 1);
+// f(i32,16, 2);
+// f(i32,16, 4);
+// f(i32,16, 8);
 f(i32,16,16);
 
-f(f32,16, 1);
-f(f32,16, 2);
-f(f32,16, 4);
-f(f32,16, 8);
+// f(f32,16, 1);
+// f(f32,16, 2);
+// f(f32,16, 4);
+// f(f32,16, 8);
 f(f32,16,16);
+// #endif
+
+// #ifdef __AVX512BW__
+// f(i16,32, 1);
+// f(i16,32, 2);
+// f(i16,32, 4);
+f(i16,32, 8);
+// #endif
+
+#if 1
+// #ifdef __AVX512FP16__
+// #ifdef __AVX512VL__
+// f(f16,16, 1);
+// f(f16,16, 2);
+// f(f16,16, 4);
+// f(f16,16, 8);
+f(f16,16,16);
+// #endif
+// f(f16,32, 1);
+// f(f16,32, 2);
+// f(f16,32, 4);
+f(f16,32, 8);
+// #endif
 #endif
 
+#if 0
+// #ifdef __AVX10_2__
+// f(bf16,16, 1);
+// f(bf16,16, 2);
+// f(bf16,16, 4);
+// f(bf16,16, 8);
+f(bf16,16,16);
+
+// f(bf16,32, 1);
+// f(bf16,32, 2);
+// f(bf16,32, 4);
+f(bf16,32, 8);
+// #endif
+#endif
+
+#if 0
 void f_i32(i32 *__restrict x, i32 *__restrict y) {
     MCA(f_i32,
         for (auto i = 0; i < 256; i++)
@@ -68,3 +119,28 @@ void f_f32(f32 *__restrict x, f32 *__restrict y) {
              x[i] += (y[i] - x[i]) / (1<<16);
     )
 }
+
+void f_i16(i16 *__restrict x, i16 *__restrict y) {
+    MCA(f_i16,
+        for (auto i = 0; i < 256; i++)
+             x[i] += (y[i] - x[i]) >> 8;
+    )
+}
+
+// void f_f16(f16 *__restrict x, f16 *__restrict y) {
+//     MCA(f_f16,
+//         for (auto i = 0; i < 256; i++)
+//              x[i] += (y[i] - x[i]) / (1<<8);
+//     )
+// }
+
+// #ifdef __AVX10_2__
+// void f_bf16(bf16 *__restrict x, bf16 *__restrict y) {
+//     MCA(f_bf16,
+//         for (auto i = 0; i < 256; i++)
+//              x[i] += (y[i] - x[i]) / (1<<8);
+//     )
+// }
+// #endif
+
+#endif
