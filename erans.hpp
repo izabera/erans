db27270e70715507c47639eb6f2af9bb4df5af9e
Isabella Bosia <izaberina@gmail.com>
2026-05-08 04:31:13 +0100

    undo

--- erans.hpp
+++ erans.hpp
@@ -5,6 +5,4 @@
 void erans_encode(std::string_view in, std::string& out);
 void erans_decode(std::string_view in, std::string& out);
 
-#define erans_stream_count 4
-#define erans_stream_maxsize (1ull<<24)
-#define erans_maxsize (erans_stream_count * erans_stream_maxsize)
+#define erans_maxsize (1ull<<24)
